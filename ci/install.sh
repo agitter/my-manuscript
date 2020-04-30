f3d7c5adf60ac98f5d76f7c29713e76787c274f8
Anthony Gitter <gitter@biostat.wisc.edu>
2020-04-30 07:46:30 -0500

    Switch to custom fork of spellcheck filter

--- ci/install.sh
+++ ci/install.sh
@@ -23,5 +23,5 @@ conda activate manubot
 if [ "${SPELLCHECK:-}" = "true" ]; then
   sudo apt-get update -y
   sudo apt-get install -y aspell aspell-en
-  wget https://raw.githubusercontent.com/pandoc/lua-filters/1c553017ecc58914c22bf2372902dca4a456929b/spellcheck/spellcheck.lua
+  wget https://raw.githubusercontent.com/agitter/lua-filters/14b142760d3a6a2c673dce1e82243cfb11b86bc8/spellcheck/spellcheck.lua
 fi
